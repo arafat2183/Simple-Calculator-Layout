809fc48197aeeae4697b52df716f7c1d29c2a956
MD ARAFAT HOSSAIN AKASH <akash076987@gmail.com>
2023-07-11 19:04:38 +0900

    Initial commit

---
+++ Simple Calculator Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Calculator Layout
+//
+//  Created by Akash Arafat on 2023/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
